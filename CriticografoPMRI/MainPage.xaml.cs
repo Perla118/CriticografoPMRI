45a4600de26cb483e4e8d93578a3f9b6f25fafe3
Perlar118 <perlamaria9545@gmail.com>
2024-01-15 09:00:59 -0700

    Agregar archivos de proyecto.

---
+++ CriticografoPMRI/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CriticografoPMRI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string nombre = Nombre.Text;
+
+            string gen = (Hombre.IsChecked ? "hombre" : "mujer");
+
+
+            var cosas = new System.Collections.Generic.List<string>();
+
+
+            if (ALTO.IsChecked)
+                cosas.Add(gen == "hombre" ? "alto" : "alta");
+
+            if (FEO.IsChecked)
+                cosas.Add(gen == "hombre" ? "feo" : "fea");
+
+            if (LISTO.IsChecked)
+                cosas.Add(gen == "hombre" ? "listo" : "lista");
+
+            if (EXTRAVAGANTE.IsChecked)
+                cosas.Add(gen == "hombre" ? "extravagante" : "extravagante");
+
+            if (RARO.IsChecked)
+                cosas.Add(gen == "hombre" ? "raro" : "rara");
+
+            if (GRANDE.IsChecked)
+                cosas.Add(gen == "hombre" ? "grande" : "grande");
+
+            string critica = $"{nombre} es {string.Join(", ", cosas)}";
+
+            Resultado.Text = critica;
+        }
+    }
+}
